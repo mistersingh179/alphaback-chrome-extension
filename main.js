39ffb9bb520bfa3fc05250fa562db704b43e7eb8
Mister Singh <mistersingh179@gmail.com>
2022-06-15 10:50:10 -0400

    initial commit with v0.0.1 pushed

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("in main.js");
+
+chrome.tabs.getCurrent(function (tab) {
+  if(tab && tab.id){
+    try{
+      chrome.tabs.update(tab.id, {
+        "url": "http://app.alphaback.xyz/showcase",
+      });
+    }catch(e){
+      console.error("unable to update tab url: ", e)
+    }
+  }
+});
