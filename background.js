8d19839a378731010dd139bfe54533d449625917
Mister Singh <mistersingh179@gmail.com>
2022-09-14 15:42:26 -0400

    upgraded to v0.2
    app is now in build directory
    also using background.js to setup uninstall link

---
+++ background.js
@@ -0,0 +1,22 @@
+console.log("this is background.js");
+
+chrome.runtime.onInstalled.addListener(async (details) => {
+  console.log("OnInstalled event: ", details, details?.reason);
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    // Code to be executed on first install
+    // eg. open a tab with a url
+    let url = chrome.runtime.getURL("/build/index.html#/how-it-works");
+    console.log('let us show them the works: ', url);
+    let tab = await chrome.tabs.create({ url });
+    console.log(`Created tab ${tab.id}`);
+    chrome.runtime.setUninstallURL("https://leaderboard.alphaback.xyz/uninstall");
+  } else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+    console.log("we have been updated from: ", details?.previousVersion)
+    chrome.runtime.setUninstallURL("https://leaderboard.alphaback.xyz/uninstall");
+    // When extension is updated
+  } else if (details.reason === chrome.runtime.OnInstalledReason.CHROME_UPDATE) {
+    // When browser is updated
+  } else if (details.reason === chrome.runtime.OnInstalledReason.SHARED_MODULE_UPDATE) {
+    // When a shared module is updated
+  }
+});
--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-console.log("in main.js");
+console.log("in main.js", chrome.runtime);
+document.getElementById("foo").innerHTML="Main.js";
 
-chrome.tabs.getCurrent(function (tab) {
-  if(tab && tab.id){
-    try{
-      chrome.tabs.update(tab.id, {
-        "url": "http://app.alphaback.xyz/showcase",
-      });
-    }catch(e){
-      console.error("unable to update tab url: ", e)
-    }
-  }
-});
+// chrome.tabs.getCurrent(function (tab) {
+//   if(tab && tab.id){
+//     try{
+//       chrome.tabs.update(tab.id, {
+//         "url": "http://app.alphaback.xyz/showcase",
+//       });
+//     }catch(e){
+//       console.error("unable to update tab url: ", e)
+//     }
+//   }
+// });
